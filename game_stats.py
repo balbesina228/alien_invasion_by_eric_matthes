456e62d8984a0f8b6c694084d7d1e382c0d8efcb
balbesina228 <109335756+balbesina228@users.noreply.github.com>
2022-08-25 11:42:58 +0700

    Add files via upload
    Added life.bmp (without it the game won't start)
    Fixed a bug: if you lose all the lives and start the game in the same window, your level was the same as you have played recently. Now it resets.

--- game_stats.py
+++ game_stats.py
@@ -7,8 +7,8 @@ class GameStats():
         with open('high_score.txt', 'r') as f:
             f = f.read()
             self.high_score = int(f)
-        self.level = 1
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
